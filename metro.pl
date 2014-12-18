62d7322463605d4c2454fe51ae2be2078ddd4b03
António Silva <antoniosilva1017@gmail.com>
2014-12-18 16:53:39 +0000

    Inicio da modelação das linhas do metro #1

---
+++ metro.pl
@@ -0,0 +1,30 @@
+linha(1, ["Château de Vincennes", "Bérault", "Saint-Mandlé-Tourelle", "Porte de Vincennes", "Nation", "Reuilly-Diderot", "Gare de Lyon", "Bastille", "Saint-Paul", "Hotel de Ville", "Châtelet", "Louvre-Rivoli", "Palais Royal-Museé du Louvre", "Tuileries", "Concorde", "Champs-Élysées-Clemenceau", "Franklin D.Roosevelt", "George V", "Charles de Gaulle - Étoile", "Argentine", "Porte Maillot", "Les Sablons", "Pont de Neuilly", "Esplanade de La Défense", "La Défense"]).
+
+linha(2, ["Porte Dauphine", "Victor Hugo", "Charles de Gaulle Étoile", "Ternes", "Courcelles", "Monceau", "Villiers", "Rome", "Place de Clichy", "Blanche", "Pigalle", "Anvers", "Barbès-Rochechouart", "La Chapelle", "Stalingrad", "Jaurès", "Colonel Fabien", "Belleville", "Couronnes", "Ménilmontant", "Père Lachaise", "Philippe Auguste", "Alexandre Dumas", "Avron", "Nation"]).
+
+linha(3, ["Pont de Levallois Bécon", "Anatole France", "Louise Michel", "Porte de Champerret", "Pereire", "Wagram", "Malesherbes", "Villiers", "Europe", "Saint-Lazare", "Havre-Caumartin", "Opéra", "Quatre-Septembre", "Bourse", "Sentier", "Réamur-Sébastopol", "Arts et Métiers", "Temple", "République", "Parmentier", "Rue Saint-Maur", "Père Lachaise", "Gambetta", "Porte de Bagnolet", "Gallieni"]).
+
+%linha 3 bis
+
+linha(4, ["Porte de Clignancourt", "Simplon", "Marcadet Poissonniers", "Château Rouge", "Barbès-Rochechouart", "Gare du Nord", "Gare de l'Est", "Château d'Eau", "Strasbourg Saint-Denis", "Réamur-Sébastopol", "Étienne Marcel", "Les Halles", "Châtelet", "Cité", "Saint-Michel", "Odéon", "Saint-Germain-des-Prés", "Saint-Sulpice", "Saint-Placide", "Montparnasse Bienvenue", "Vavin", "Raspail", "Denfert-Rochereau", "Mouton-Duvernet", "Alésia", "Porto d'Orléans"]).
+
+linha(5, ["Bobigny Pablo Picasso", "Bobigny-Pantin", "Hoche", "Porte de Pantin", "Ourcq", "Laumière", "Jaurès", "Stalingrad", "Gare du Nord", "Gare de l'Est", "Jacques Bonsergent", "République", "Oberkampf", "Richard-Lenoir", "Bréguet-Sabin", "Bastille", "Quai de la Rapée", "Gare d'Austerlitz", "Saint-Marcel", "Campo-Formio", "Place d'Italie"]).
+
+linha(6, ["Charles de Gaulle Étoile", "Kléber", "Boissière", "Trocadéro", "Passy", "Bir-Hakeim"], "Dupleix", "La Motte-Picquet Grenelle", "Cambronne", "Sèvres-Lecourbe", "Pasteur", "Montparnasse Bienvenue", "Edgar Quinet", "Raspail", "Denfert-Rochereau", "Saint-Jacques", "Glacière", "Corvisart", "Place d'Italie", "Nationale", "Chevaleret", "Quai de la Gare", "Bercy", "Dugommier", "Daumesnil", "Bel-Air", "Picpus", "Nation").
+
+linha(7,["La Courneuve 8 Mai 1945", "Fort d'Aubervilliers", "Aubervilliers-Pantin Quatre Chemins", "Porte de la Villette", "Corentin Cariou", "Crimée", "Riquet", "Stalingrad", "Louis Blanc", "Château-Landon", "Gare de l'Est", "Poissonnière", "Cadet", "Le Peletier", "Chaussée d'Antin-La Fayette", "Opéra", "Pyramides", "Palais Royal", "Musée du Louvre", "Pont Neuf", "Châtelet", "Pont Marie", "Sully-Morland", "Jussieu", "Place Monge", "Censier-Daubenton", "Les Gobelins", "Place d'Italie", "Tolbiac", "Maison Blanche", "Porte d'Italie", "Porte de Choisy", "Porte d'Ivry", "Pierre Curie", "Mairie d'Ivry"]).
+
+linha(7,["La Courneuve 8 Mai 1945", "Fort d'Aubervilliers", "Aubervilliers-Pantin Quatre Chemins", "Porte de la Villette", "Corentin Cariou", "Crimée", "Riquet", "Stalingrad", "Louis Blanc", "Château-Landon", "Gare de l'Est", "Poissonnière", "Cadet", "Le Peletier", "Chaussée d'Antin-La Fayette", "Opéra", "Pyramides", "Palais Royal", "Musée du Louvre", "Pont Neuf", "Châtelet", "Pont Marie", "Sully-Morland", "Jussieu", "Place Monge", "Censier-Daubenton", "Les Gobelins", "Place d'Italie", "Tolbiac", "Maison Blanche", "Le Kremlin-Bicêtre", "Villejuif Léo-Lagrange", "Villejuif Paul Vaillant-Couturier", "Villejuif Louis Aragon"]).
+
+linha(8, ["Ballard", "Lourmel", "Boucicaut", "Félix Faure", "Commerce", "La Motte-Picquet-Grenelle", "École Militaire", "La Tour-Maubourg", "Invalides", "Concorde", "Madeleine", "Opéra", "Richelieu-Drouot", "Grands Boulevards", "Bonne Nouvelle", "Strasbourg-Saint-Dennis", "République", "Filles du Calvaire", "Saints-Sébastien-Froissart", "Chemin Vert", "Bastille", "Ledru-Rollin", "Faidherbe-Chaligny", "Reuilly-Diderot", "Montgallet", "Daumesnil", "Michel Bizot", "Porte Dorée", "Porte de Charenton", "Liberté", "Charenton-Écoles", "École Vétérinaire de Maisons-Alfort", "Maisons-Alfort-Stade", "Maisons-Alfort-Les Juilliottes", "Créteil-L'Échat", "Créteil-Université", "Créteil-Préfecture"]).
+
+linha(9, ["Pont de Sèvres", "Billancourt", "Marcel Sembat", "Porte de Saint-Cloud", "Exelmans", "Michel-Ange-Auteuil", "Jasmin", "Ranelagh", "La Muette", "Rue de la Pompe", "Trocadéro", "Iéna", "Alma-Marceau", "Franklin D.Roosevelt", "Saint-Philippe-du-Roule", "Miromesnil", "Saint-Augustin", "Havre-Caumartin", "Chaussée d'Antin-La Fayette", "Richelieu-Drouot", "Grands Boulevards", "Bonne Nouvelle", "Strasbourg-Saint-Dennis", "République", "Oberkampf", "Saint-Ambroise", "Voltaire", "Charonne", "Rue des Boulets", "Nation", "Buzenval", "Maraichers", "Porte de Montreuil", "Robespierre", "Croix de Chavaux", "Mairie de Montreuil"]).
+
+linha(10, ["Boulogne Pont de Saint-Cloud", "Boulogne Jean Jaurès", "Michel-Ange Molitor", "Chardon Lagache"]).
+
+
+
+
+
+
+
