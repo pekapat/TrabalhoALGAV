0e027822db206192bfacdd1e301b060d98718262
António Silva <antoniosilva1017@gmail.com>
2014-12-20 18:26:20 +0000

    Implementado o predicado gera_estacoes, que cria factos do tipo estacoes(Estacao). #2

--- metro.pl
+++ metro.pl
@@ -10,17 +10,30 @@ linha(4, ["Porte de Clignancourt", "Simplon", "Marcadet Poissonniers", "Château
 
 linha(5, ["Bobigny Pablo Picasso", "Bobigny-Pantin", "Hoche", "Porte de Pantin", "Ourcq", "Laumière", "Jaurès", "Stalingrad", "Gare du Nord", "Gare de l'Est", "Jacques Bonsergent", "République", "Oberkampf", "Richard-Lenoir", "Bréguet-Sabin", "Bastille", "Quai de la Rapée", "Gare d'Austerlitz", "Saint-Marcel", "Campo-Formio", "Place d'Italie"]).
 
-linha(6, ["Charles de Gaulle Étoile", "Kléber", "Boissière", "Trocadéro", "Passy", "Bir-Hakeim"], "Dupleix", "La Motte-Picquet Grenelle", "Cambronne", "Sèvres-Lecourbe", "Pasteur", "Montparnasse Bienvenue", "Edgar Quinet", "Raspail", "Denfert-Rochereau", "Saint-Jacques", "Glacière", "Corvisart", "Place d'Italie", "Nationale", "Chevaleret", "Quai de la Gare", "Bercy", "Dugommier", "Daumesnil", "Bel-Air", "Picpus", "Nation").
+linha(6, ["Charles de Gaulle Étoile", "Kléber", "Boissière", "Trocadéro", "Passy", "Bir-Hakeim", "Dupleix", "La Motte-Picquet Grenelle", "Cambronne", "Sèvres-Lecourbe", "Pasteur", "Montparnasse Bienvenue", "Edgar Quinet", "Raspail", "Denfert-Rochereau", "Saint-Jacques", "Glacière", "Corvisart", "Place d'Italie", "Nationale", "Chevaleret", "Quai de la Gare", "Bercy", "Dugommier", "Daumesnil", "Bel-Air", "Picpus", "Nation"]).
 
 linha(7,["La Courneuve 8 Mai 1945", "Fort d'Aubervilliers", "Aubervilliers-Pantin Quatre Chemins", "Porte de la Villette", "Corentin Cariou", "Crimée", "Riquet", "Stalingrad", "Louis Blanc", "Château-Landon", "Gare de l'Est", "Poissonnière", "Cadet", "Le Peletier", "Chaussée d'Antin-La Fayette", "Opéra", "Pyramides", "Palais Royal", "Musée du Louvre", "Pont Neuf", "Châtelet", "Pont Marie", "Sully-Morland", "Jussieu", "Place Monge", "Censier-Daubenton", "Les Gobelins", "Place d'Italie", "Tolbiac", "Maison Blanche", "Porte d'Italie", "Porte de Choisy", "Porte d'Ivry", "Pierre Curie", "Mairie d'Ivry"]).
 
-linha(7,["La Courneuve 8 Mai 1945", "Fort d'Aubervilliers", "Aubervilliers-Pantin Quatre Chemins", "Porte de la Villette", "Corentin Cariou", "Crimée", "Riquet", "Stalingrad", "Louis Blanc", "Château-Landon", "Gare de l'Est", "Poissonnière", "Cadet", "Le Peletier", "Chaussée d'Antin-La Fayette", "Opéra", "Pyramides", "Palais Royal", "Musée du Louvre", "Pont Neuf", "Châtelet", "Pont Marie", "Sully-Morland", "Jussieu", "Place Monge", "Censier-Daubenton", "Les Gobelins", "Place d'Italie", "Tolbiac", "Maison Blanche", "Le Kremlin-Bicêtre", "Villejuif Léo-Lagrange", "Villejuif Paul Vaillant-Couturier", "Villejuif Louis Aragon"]).
+linha("7bis",["La Courneuve 8 Mai 1945", "Fort d'Aubervilliers", "Aubervilliers-Pantin Quatre Chemins", "Porte de la Villette", "Corentin Cariou", "Crimée", "Riquet", "Stalingrad", "Louis Blanc", "Château-Landon", "Gare de l'Est", "Poissonnière", "Cadet", "Le Peletier", "Chaussée d'Antin-La Fayette", "Opéra", "Pyramides", "Palais Royal", "Musée du Louvre", "Pont Neuf", "Châtelet", "Pont Marie", "Sully-Morland", "Jussieu", "Place Monge", "Censier-Daubenton", "Les Gobelins", "Place d'Italie", "Tolbiac", "Maison Blanche", "Le Kremlin-Bicêtre", "Villejuif Léo-Lagrange", "Villejuif Paul Vaillant-Couturier", "Villejuif Louis Aragon"]).
 
 linha(8, ["Ballard", "Lourmel", "Boucicaut", "Félix Faure", "Commerce", "La Motte-Picquet-Grenelle", "École Militaire", "La Tour-Maubourg", "Invalides", "Concorde", "Madeleine", "Opéra", "Richelieu-Drouot", "Grands Boulevards", "Bonne Nouvelle", "Strasbourg-Saint-Dennis", "République", "Filles du Calvaire", "Saints-Sébastien-Froissart", "Chemin Vert", "Bastille", "Ledru-Rollin", "Faidherbe-Chaligny", "Reuilly-Diderot", "Montgallet", "Daumesnil", "Michel Bizot", "Porte Dorée", "Porte de Charenton", "Liberté", "Charenton-Écoles", "École Vétérinaire de Maisons-Alfort", "Maisons-Alfort-Stade", "Maisons-Alfort-Les Juilliottes", "Créteil-L'Échat", "Créteil-Université", "Créteil-Préfecture"]).
 
 linha(9, ["Pont de Sèvres", "Billancourt", "Marcel Sembat", "Porte de Saint-Cloud", "Exelmans", "Michel-Ange-Auteuil", "Jasmin", "Ranelagh", "La Muette", "Rue de la Pompe", "Trocadéro", "Iéna", "Alma-Marceau", "Franklin D.Roosevelt", "Saint-Philippe-du-Roule", "Miromesnil", "Saint-Augustin", "Havre-Caumartin", "Chaussée d'Antin-La Fayette", "Richelieu-Drouot", "Grands Boulevards", "Bonne Nouvelle", "Strasbourg-Saint-Dennis", "République", "Oberkampf", "Saint-Ambroise", "Voltaire", "Charonne", "Rue des Boulets", "Nation", "Buzenval", "Maraichers", "Porte de Montreuil", "Robespierre", "Croix de Chavaux", "Mairie de Montreuil"]).
 
-linha(10, ["Boulogne Pont de Saint-Cloud", "Boulogne Jean Jaurès", "Michel-Ange Molitor", "Chardon Lagache"]).
+linha(10, ["Boulogne Pont de Saint-Cloud", "Boulogne Jean Jaurès", "Michel-Ange Molitor", "Chardon Lagache", "MiraBeau", "Javel-André Citroen", "Charles Michels", "Avenue Émile Zola", "La Motte-Picquet Grenelle", "Ségur", "Duroc", "Vaneau", "Sèvres-Babylone", "Mabillon", "Odéon", "Cluny-La Sorbonne", "Maubert-Mutualité", "Cardinal Lemoine", "Jussieu", "Gare d'Austerlitz"]).
+
+%	Faltam as linhas 10.2, 11, 12, 13, 14, 3bis
+
+gera_estacoes:- findall(L, linha(_,L), LE), gera_estacoes(LE, Estacoes), gera_estacoes(Estacoes).
+
+gera_estacoes([]).
+gera_estacoes([H|T]):- gera_estacoes(T), atom_string(H, E), assertz(estacoes(E)).
+
+gera_estacoes([],[]).
+gera_estacoes([H|T], LE):- gera_estacoes(T,LR), append(LR, H, LE).
+
+
+
 
 
 
